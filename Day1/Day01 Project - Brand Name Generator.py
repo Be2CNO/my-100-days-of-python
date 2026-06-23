ebc725dc51c295ffa28a6d3e850a5deb6c1a3b31
Be2CNO <prvardhan0417@gmail.com>
2026-06-23 16:23:21 +0530

    Day 01 completed

---
+++ Day1/Day01 Project - Brand Name Generator.py
@@ -0,0 +1,7 @@
+# Brand Name Generator
+
+print("Privet")
+city_name = input("Enter Yout City Name :")
+name = input("enter your name:")
+brand = name + city_name
+print(brand)
---
+++ Day1/day01_print.py
@@ -0,0 +1,40 @@
+# Day 1 - 100 Days Of Python 
+
+print("Hell World")
+print("Hell World")
+print("Hell World")
+print("Hell World")
+print("Hell World")
+print("Hell World")
+print("Hell World")
+print("Hell World")
+
+# Or You simply use \n for moving to next line
+
+print("Hell\nHell\nHell")
+
+# add (+) symbol work
+print("hell" + "world")  # It Sticks Both Strings
+
+# For adding space use coma (,)
+print("hell","world")
+
+# Input Function - It takes data from user
+input("Who says \"daddy's home\" ?")
+
+# Input funtion inside the print function
+
+print("Hello", input("Who says \"daddy's home\" ?"))
+# or a alternate way
+print("Hello " + input("Who says \"daddy's home\" ?"))
+
+# Varibles
+
+name = input("What's your name? ")
+length = len(name) # Tells the no. of characters in it
+print(length)
+
+
+print("In Shades Of \"Darkness\"  ")
+
+print("In Shades Of \"Darkness\"  ")
